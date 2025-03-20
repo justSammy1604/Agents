5a5001ed0660ff49ebf2a3cf7aebe473c65fe498
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2025-03-20 07:07:37 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,26 @@
+from llama_index.llms.gemini import Gemini
+from llama_index.llms.cohere import Cohere
+from llama_index.core.agent import ReActAgent
+from llama_index.core.tools import FunctionTool
+
+llm = Cohere(model='command', api_key=API_KEY_C)
+
+def mult(a:float, b:float) -> float:
+  return a*b
+
+def add(a:float, b:float) -> float:
+  return a+b
+
+def sub(a:float, b:float) -> float:
+  return a-b
+
+def div(a:float, b:float) -> float:
+  return a//b
+
+add_tool = FunctionTool.from_defaults(fn=add)
+sub_tool = FunctionTool.from_defaults(fn=sub)
+mult_tool = FunctionTool.from_defaults(fn=mult)
+div_tool = FunctionTool.from_defaults(fn=div)
+
+tools_list = [add_tool, sub_tool, mult_tool, div_tool]\
+
